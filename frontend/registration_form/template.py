d87c91b373f8b16a6de7438de41f16c5692a3847
kruti-s <67128816+kruti-s@users.noreply.github.com>
2021-01-09 18:29:27 -0500

    Preliminary form
    Pretty rudimentary
    I'm not sure if it's working right now

---
+++ frontend/registration_form/template.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+class AddressForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Name'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput())
+  
+
+
+    
