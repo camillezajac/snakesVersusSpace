32c393907402c7a92e3696067ff7ba2faf163385
Camille Zajac <19757948+camillezajac@users.noreply.github.com>
2021-01-08 17:20:58 -0800

    Delete views.py

--- snake/snakevsspace/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# handle request sent when visitor goes to homepage
-def home(request):
-	return HttpResponse('<h1>Snakes vs Space</h1>')
