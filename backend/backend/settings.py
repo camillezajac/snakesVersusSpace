4f135646fe6d99a206f39563c681ff2475ce3487
Kumar Mallikarjuna <kumarmallikarjuna1@gmail.com>
2021-01-10 16:55:23 +0530

    Load Bootstrap

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,8 +33,10 @@ INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
     'django.contrib.messages', 'django.contrib.staticfiles', 'rest_framework',
-    'kernel', 'register'
+    'crispy_forms', 'kernel', 'register'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
